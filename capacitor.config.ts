54739f165b4bfc0e33d3c5f0a1abc61ff2a16a1f
Evgenii Zorin <zorin.evgenii.m@gmail.com>
2022-08-15 22:16:45 -0500

    first version

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '220815_app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
